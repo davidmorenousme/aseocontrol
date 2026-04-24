fdc6f01ae175c41bdfdba495cda0a2d62aae59df
davidmorenousme <120667885+davidmorenousme@users.noreply.github.com>
2026-04-24 18:19:57 -0500

    PORT Update

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ MYSQL_HOST     = os.getenv('MYSQL_HOST', 'localhost')
 MYSQL_USER     = os.getenv('MYSQL_USER', 'root')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
 MYSQL_DB       = os.getenv('MYSQL_DB', 'aseocontrol_db')
+MYSQL_PORT     = os.getenv('MYSQL_PORT', 3306)
 MYSQL_CURSORCLASS = 'DictCursor'
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
